8bebca629347bec677984d63b36627990be507f2
EmilWallman <emil.wallman@elev.ga.ntig.se>
2021-11-29 22:41:51 +0100

    The final stuff

--- slideshow.js
+++ slideshow.js
@@ -1,33 +1,18 @@
-//For the slideshow
-var slideIndex = 1;
-showDivs(slideIndex);
+var slideIndex = 0;
 
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length){
-    slideIndex = 1
-  }
-  if (n < 1){
-    slideIndex = x.length
-  }
-  for (i = 0; i < x.length; i++){
-    x[i].style.display = "none";
-  }
-  x[slideIndex-1].style.display = "block";
-}
-
-
-
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
